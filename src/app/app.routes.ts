6d0fcca8b96458f4326fc128905ce11aff13c31c
Cyril <cda.skills@gmail.com>
2024-06-30 09:36:49 +0200

    RouterLinks fonctionnels

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {AcceuilComponent} from "./appli/acceuil/acceuil.component"
+import {ConnectionComponent} from './appli/connection/connection.component'
+import {InscriptionComponent} from './appli/inscription/inscription.component'
+import {PresentationComponent} from './appli/presentation/presentation.component'
+import {ContactComponent} from './appli/contact/contact.component'
+import {NotFoundComponent} from './appli/not-found/not-found.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	
+	{ path: '/', component: AcceuilComponent, },
+    { path: '/login', component: ConnectionComponent},
+    { path: 'signup', component: InscriptionComponent },
+    { path: 'about', component: PresentationComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: NotFoundComponent }
+];
