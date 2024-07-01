fd235c6682fc2c171607f26fc8e86f7acefef074
Cyril <cda.skills@gmail.com>
2024-07-01 09:41:04 +0200

    Formulaire d'inscription
    4 informations( Pseudo , Email , Mot de passe , Newsletter)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,20 @@
 import { Routes } from '@angular/router';
-import {AcceuilComponent} from "./appli/acceuil/acceuil.component"
-import {ConnectionComponent} from './appli/connection/connection.component'
-import {InscriptionComponent} from './appli/inscription/inscription.component'
-import {PresentationComponent} from './appli/presentation/presentation.component'
-import {ContactComponent} from './appli/contact/contact.component'
-import {NotFoundComponent} from './appli/not-found/not-found.component'
+
+import {AcceuilComponent} from "./appli/acceuil/acceuil.component";
+import {NewsComponent} from "./appli/news/news.component";
+import {ConnectionComponent} from './appli/connection/connection.component';
+import {InscriptionComponent} from './appli/inscription/inscription.component';
+import {PresentationComponent} from './appli/presentation/presentation.component';
+import {ContactComponent} from './appli/contact/contact.component';
+
 
 export const routes: Routes = [
 	
 	{ path: '', component: AcceuilComponent },
+	{ path: 'news', component: NewsComponent},
     { path: 'login', component: ConnectionComponent },
     { path: 'signup', component: InscriptionComponent },
     { path: 'about', component: PresentationComponent },
-    { path: 'contact', component: ContactComponent },
-    { path: '**', component: NotFoundComponent }
+    { path: 'contact', component: ContactComponent }
+   
 ];
